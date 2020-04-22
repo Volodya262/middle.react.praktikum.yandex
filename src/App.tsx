120d5cae058eeb80b56c56fd7bbda87aac217ffa
Vladimir Gorbatov <volodya_26@mail.ru>
2020-04-22 21:15:30 +0500

    my first react commit (not really)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./header";
+import Description from "./Description";
 
 function App() {
   return (
     <div className="App">
+      <Header name={'user'}/>
+      <Description/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
