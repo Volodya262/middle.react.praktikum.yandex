be655c3fd63107e079cf9593037d512eef34db08
Vladimir Gorbatov <volodya_26@mail.ru>
2020-04-28 23:03:02 +0500

    Updates

---
+++ precache-manifest.5bb93fee36b5e83e926177a671233eb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410d6fa225ea8a64acab6dcdab119dc3",
+    "url": "/middle.react.praktikum.yandex/index.html"
+  },
+  {
+    "revision": "93b6660da0858e7c6cc7",
+    "url": "/middle.react.praktikum.yandex/static/css/main.1f0d7f84.chunk.css"
+  },
+  {
+    "revision": "f5577ba17342bb22368c",
+    "url": "/middle.react.praktikum.yandex/static/js/2.19cd454e.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/middle.react.praktikum.yandex/static/js/2.19cd454e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b6660da0858e7c6cc7",
+    "url": "/middle.react.praktikum.yandex/static/js/main.59486dff.chunk.js"
+  },
+  {
+    "revision": "4decdeb8204cfe9224a2",
+    "url": "/middle.react.praktikum.yandex/static/js/runtime-main.c0119f50.js"
+  }
+]);
