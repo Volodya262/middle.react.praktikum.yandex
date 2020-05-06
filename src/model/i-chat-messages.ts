6e49974945e1601ba9ba17822d4f74eba3ca3c2f
Vladimir Gorbatov <volodya_26@mail.ru>
2020-05-06 18:07:35 +0500

    Послание код-ревьюеру)))))

--- src/model/i-chat-messages.ts
+++ src/model/i-chat-messages.ts
@@ -1,5 +1,7 @@
 import {IMessage} from "./i-message";
 
+// Если это не модель, то что это? Куда класть типы? В src/types? Чем types отличаются от model?
+// Речь идет о разделении контрактов на Application и Business уровни?
 export interface IChatMessages {
     /**
      * id чата, которому принадлежат сообщения
